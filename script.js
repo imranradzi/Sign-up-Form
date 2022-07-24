22f884ce80197967cfe901132c45346fc79b0d80
Imran <imranradziii@gmail.com>
2022-07-24 23:23:54 +0100

    added code to script.js so that form doesn't submit if passwords don't match, and also display error message. made some cosmetic changes to how the inputs behave when focused, hovered, etc.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let password = document.querySelector('#password');
 let confirmPassword = document.querySelector('#confirmpassword');
+let messageRow = document.querySelector('.row4');
+let button = document.querySelector('button');
 
 function checkError() {
   console.log(password);
@@ -9,12 +11,21 @@ function checkError() {
     console.log('right');
     password.setAttribute('class', 'correct');
     confirmPassword.setAttribute('class', 'correct');
+    messageRow.setAttribute('class', 'correct')
+    messageRow.textContent = 'Passwords Match!'
   } else {
     console.log('wrong')
     password.setAttribute('class', 'error');
     confirmPassword.setAttribute('class', 'error');
+    messageRow.setAttribute('class', 'error')
+    messageRow.textContent = 'Passwords Do Not Match!'
   }
 }
 
 password.addEventListener('focusout', checkError);
 confirmPassword.addEventListener('focusout', checkError);
+button.addEventListener('click', (e) => {
+  if (pass1 !== pass2) {
+    e.preventDefault();
+  }
+})
